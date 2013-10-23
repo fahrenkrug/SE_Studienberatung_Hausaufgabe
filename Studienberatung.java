5475255d9eecfc3c699860a7a60deb9387bae6d2
Finn Fahrenkrug <finnfahrenkrug@gmail.com>
2013-10-23 15:51:34 +0200

    Added Abiturfrage and answr from scanner

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,5 +1,7 @@
 public class Studienberatung{
 	public static void main (String[] args){
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Hast du Abitur? [j/n]");
+		String answer = sc.next();
 	}
 }
