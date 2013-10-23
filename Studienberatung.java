770760982e4b549abbd17a84e4cdcb89ca0c73e7
Finn Fahrenkrug <finnfahrenkrug@gmail.com>
2013-10-23 16:28:49 +0200

    Implemented berufsausbildung

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,5 +3,20 @@ public class Studienberatung{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Hast du Abitur? [j/n]");
 		String answer = sc.next();
+		
+		if (answer.equalsIgnoreCase("n"){
+			System.out.println("Hast du Fachabitur? [j/n]");
+			String answer2 = sc.next();
+			if (answer2.equalsIgnoreCase("j"){
+				System.out.println("Hast du eine Berufsausbildung? [j/n]");
+				String answer4 = sc.next()
+				if (answer4.equalsIgnoreCase("j"){
+					System.out.println("Du kannst ja später noch studieren");
+				} 
+				else if (answer4.equalsIgnoreCase("n"){
+					System.out.println("Besser noch studieren");
+				}
+			}
+		}		
 	}
 }
