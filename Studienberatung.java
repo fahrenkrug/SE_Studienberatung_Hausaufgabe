602fd9f553a4feeb1a23181e3f77f454a708f24a
Finn Fahrenkrug <finnfahrenkrug@gmail.com>
2013-10-23 16:12:59 +0200

    Implemented bist-du-wissbegierig

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,5 +3,16 @@ public class Studienberatung{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Hast du Abitur? [j/n]");
 		String answer = sc.next();
+		
+		if (answer.equalsIgnoreCase("j"){
+			System.out.println("Bist du wissbegierig? [j/n]");
+			String answer3 = sc.next();
+			if (answer3.equalsIgnorease("j"){
+				System.out.println("Auf jeden Fall studieren");
+			}
+			else if (answer.equalsIgnoreCase("n"){
+				System.out.println("Nicht studieren");
+			}
+		}
 	}
 }
