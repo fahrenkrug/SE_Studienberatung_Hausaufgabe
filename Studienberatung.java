c3b548a251f8f106d6ddae183f59f96e5ca338d2
Finn Fahrenkrug <finnfahrenkrug@gmail.com>
2013-10-23 16:06:16 +0200

    Implemented hast-du-fachabitur

--- Studienberatung.java
+++ Studienberatung.java
@@ -3,5 +3,13 @@ public class Studienberatung{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Hast du Abitur? [j/n]");
 		String answer = sc.next();
+		
+		if (answer.equalsIgnoreCase("n"){
+			System.out.println("Hast du Fachabitur? [j/n]");
+			String answer2 = sc.next();
+			if (answer2.equalsIgnoreCase("n"){
+				System.out.println("Nicht studieren");
+			}
+		}
 	}
 }
